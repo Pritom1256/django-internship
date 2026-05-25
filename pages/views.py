6672fe83d6fbdc9123fa5e12e89362ccac5aa5a4
Anindo Mazumder <Anindo mazumder@gmail.com>
2026-05-24 21:25:48 -0700

    Django Part 2 completed - templating project

--- pages/views.py
+++ pages/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
 
-
 def home(request):
     context = {
         'name': 'Pritom',
         'course': 'django internship training',
         'skills': ['Python', 'Django', 'HTML', 'CSS']
     }
-
     return render(request, 'home.html', context)
 
 
 def about(request):
-    context = {
+    return render(request, 'about.html', {
         'description': 'This page demonstrates Django templating.',
         'age': 20
-    }
-
-    return render(request, 'about.html', context)
-# Create your views here.
+    })
